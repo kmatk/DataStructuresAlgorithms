a93b7c4e71d135df70e81f29480f4f054642c0bb
Kevin Atkinson <kmatk620@gmail.com>
2023-03-13 18:21:54 -0400

    Adding Graph.py and Updating BST.py

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -40,18 +40,3 @@ class BinarySearchTree:
                 return True
             
         return False
-
-
-
-my_tree = BinarySearchTree()
-my_tree.insert(47)
-my_tree.insert(21)
-my_tree.insert(76)
-my_tree.insert(18)
-my_tree.insert(27)
-my_tree.insert(52)
-my_tree.insert(82)
-
-print(my_tree.contains(27))
-print(my_tree.contains(17))
-print(my_tree.contains(82))
