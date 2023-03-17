9dd818e69bb2208bdf62a7fe8bf393df24fbfdc4
Kevin Atkinson <kmatk620@gmail.com>
2023-03-16 22:29:52 -0400

    Added recursive insert and recursive contains methods

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -39,4 +39,29 @@ class BinarySearchTree:
             else:
                 return True
             
-        return False
+    def r_contains(self, value):
+        return self.__r_contains(self.root, value)
+    
+    def __r_contains(self, current_node, value):
+        if current_node == None:
+            return False
+        if value == current_node.value:
+            return True
+        if value < current_node.value:
+            return self.__r_contains(current_node.left, value)
+        if value > current_node.value:
+            return self.__r_contains(current_node.right, value)
+        
+    def r_insert(self, value):
+        if self.root == None:
+            self.root = Node(value)
+        self.__r_insert(self.root, value)
+
+    def __r_insert(self, current_node, value):
+        if current_node == None:
+            return Node(value)
+        if value < current_node.value:
+            current_node.left = self.__r_insert(current_node.left, value)
+        if value > current_node.value:
+            current_node.right = self.__r_insert(current_node.right, value)
+        return current_node
