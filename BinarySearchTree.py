9ad1b3d39eee925126b2f32d8467c71101a4d932
Kevin Atkinson <kmatk620@gmail.com>
2023-03-18 14:19:31 -0400

    Completed delete node method

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -66,6 +66,11 @@ class BinarySearchTree:
             current_node.right = self.__r_insert(current_node.right, value)
         return current_node
     
+    def min_value(self, current_node):
+        while (current_node.left is not None):
+            current_node = current_node.left
+        return current_node.value
+
     def delete_node(self, value):
         self.__delete_node(self.root, value)
     
@@ -76,4 +81,15 @@ class BinarySearchTree:
             current_node.left = self.__delete_node(current_node.left, value)
         elif value > current_node.value:
             current_node.right = self.__delete_node(current_node.left, value)
+        else:
+            if current_node.left == None and current_node.right == None:
+                return None
+            elif current_node.left == None:
+                current_node = current_node.right
+            elif current_node.right == None:
+                current_node = current_node.left 
+            else:
+                sub_tree_min = self.min_value(current_node.right)
+                current_node.value = sub_tree_min
+                current_node.right = self.__delete_node(current_node.right, sub_tree_min)
         return current_node
