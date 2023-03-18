6e32016f5ae5964d69ef31d42002e40e391b15ec
Kevin Atkinson <kmatk620@gmail.com>
2023-03-17 23:16:59 -0400

    Added incomplete delete_node method

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -65,3 +65,15 @@ class BinarySearchTree:
         if value > current_node.value:
             current_node.right = self.__r_insert(current_node.right, value)
         return current_node
+    
+    def delete_node(self, value):
+        self.__delete_node(self.root, value)
+    
+    def __delete_node(self, current_node, value):
+        if current_node == None:
+            return None
+        if value < current_node.value:
+            current_node.left = self.__delete_node(current_node.left, value)
+        elif value > current_node.value:
+            current_node.right = self.__delete_node(current_node.left, value)
+        return current_node
